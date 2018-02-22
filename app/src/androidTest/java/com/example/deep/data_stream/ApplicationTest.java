d9afb42677758862090953272975e20222c557bc
Deepkawa <kawadeep3@gmail.com>
2018-02-22 17:39:46 +0530

    Initial Commit

---
+++ app/src/androidTest/java/com/example/deep/data_stream/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.deep.data_stream;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
